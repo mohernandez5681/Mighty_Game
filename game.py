a7b02a762e7057637d3d10205eb43adb6d848169
mohernandez5681 <115736463+mohernandez5681@users.noreply.github.com>
2022-11-09 13:53:51 -0500

    1353

--- game.py
+++ game.py
@@ -11,10 +11,15 @@ while True:
     recent_events = pygame.event.get()
     print("----------done checking for events----------")
     for event in recent_events:
+        if event.type == pygame.QUIT:
+            print("Ha Ha I will never quit")
+            pygame.quit()
+            sys.exit()
+
         if event.type == pygame.K_r:
             screen.fill((255,0,0))
         elif event.type == pygame.K_g:
             screen.fill((0,255,0))
 
     pygame.display.flip()
-    time.sleep(2)
+    time.sleep(1)
---
+++ water_game.py
@@ -0,0 +1,8 @@
+import pygame
+import sys
+import time
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("This is ")
