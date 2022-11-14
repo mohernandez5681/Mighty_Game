5af5bb95f87f7cb7e0e2cf2d71ede218546c02c2
mohernandez5681 <115736463+mohernandez5681@users.noreply.github.com>
2022-11-14 14:07:57 -0500

    1407

--- game.py
+++ game.py
@@ -3,25 +3,25 @@ import time
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((700,500))
-pygame.display.set_caption('really cool game')
-screen.fill((0,0,225))
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("This is my game!")
+screen.fill((0, 0, 255))
 
 while True:
-    print("-----------check for new events-------------")
-    recent_events = pygame.event.get()
-    print("----------done checking for events----------")
-    for event in recent_events:
-        if event.type == pygame.QUIT:
-            print("Ha Ha I will never quit")
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                screen.fill((255,0,0))
-            elif event.key == pygame.K_g:
-                screen.fill((0,255,0))
-            elif event.key == pygame.K_w:
-                screen.fill((0,0,225))
-    pygame.display.flip()
-    time.sleep(1)
+   #step 1 - check for user input (key press, mouse clicks)
+   recent_events = pygame.event.get()
+   print("-----------Checking for new events ------------")
+   for event in recent_events:
+       #print(event.type)
+       if event.type == pygame.QUIT:
+           print("Ha Ha I will never quit!")
+           pygame.quit()
+           sys.exit()
+       elif event.type == pygame.KEYUP:
+           screen.fill((255, 0, 0))
+       elif event.type == pygame.MOUSEMOTION:
+           screen.fill((0, 255, 0))
+   print("----------Done Checking -----------------")
+   pygame.display.flip()
+   time.sleep(1)
--- water_game.py
+++ water_game.py
@@ -7,17 +7,39 @@ pygame.init()
 water_image = pygame.image.load('images/water.png')
 water_rect = water_image.get_rect()
 tile_size = water_rect.width
-
-screen = pygame.display.set_mode((10 + tile_size, 10+tile_size))
+screen = pygame.display.set_mode((tile_size*10, tile_size*10))
 pygame.display.set_caption("This is my game")
 screen.fill((0,0,0))
 
+# load my island images
+top_left = pygame.image.load('images/top_left.png')
+top_mid = pygame.image.load('images/top_mid.png')
+top_right = pygame.image.load('images/top_right.png')
+bottom_left = pygame.image.load('images/bottom_left.png')
+bottom_mid = pygame.image.load('images/bottom_mid.png')
+bottom_right = pygame.image.load('images/bottom_right.png')
 
-#print(water_rect)
-#screen.blit(water_image, (168, 168))
+
 
 screen_rect = screen.get_rect()
 
+rows = screen_rect.height // tile_size
+cols = screen_rect.width // tile_size
+
+for x in range(rows):
+    for y in range(cols):
+        screen.blit(water_image, (x * water_rect.height, y * water_rect.width))
+
+screen.blit(top_left,(screen_rect.centerx - 2*tile_size, screen_rect.centery - tile_size))
+screen.blit(top_mid,(screen_rect.centerx - tile_size, screen_rect.centery - tile_size))
+screen.blit(top_right,(screen_rect.centerx, screen_rect.centery - tile_size))
+screen.blit(bottom_left,(screen_rect.centerx - 2*tile_size, screen_rect.centery))
+screen.blit(bottom_mid,(screen_rect.centerx - 1*tile_size, screen_rect.centery))
+screen.blit(bottom_right,(screen_rect.centerx, screen_rect.centery))
+
+
+
+
 
 pygame.display.flip()
-time.sleep(10)
+time.sleep(5)
